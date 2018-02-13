a6710cece309f82f88edfc55afa666dc2275dac0
Iordanov <bgdio@eur.ad.sag>
2018-02-13 18:24:29 +0200

    add data service

--- src/app/taskdata.service.ts
+++ src/app/taskdata.service.ts
@@ -1,10 +1,60 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+class BusinessData {
+  type: string;
+  name: any;
+  input: boolean;
+  output: boolean;
+  array: boolean;
+
+  constructor(type: string, name: any, input: boolean, output: boolean, array: boolean) {
+    this.type = type;
+    this.name = name;
+    this.input = input;
+    this.output = output;
+    this.array = array;
+  }
+}
+
+class TaskType {
+  name: string;
+  data: BusinessData;
+
+  constructor(name: string){
+    this.name = name;
+  }
+
+  setBusinessData(data: BusinessData){
+    this.data = data;
+  }
+}
+
 
 @Injectable()
 export class TaskdataService {
-  private tasks = new BehaviorSubject<any>(['task1','task2']);
+  // private tasks = new BehaviorSubject<any>(['task1','task2']);
+  private tasktypes = new Array<TaskType>(); //: TaskType[];
 
-  constructor() { }
+  constructor() {
+    // this.tasktypes = new Array<TaskType>();
+  }
+
+  addTaskType(task: TaskType){
+    // var task = new TaskType(string);
+    this.tasktypes.push(task);
+  }
+
+  // getTaskForName(name: string){
+  //   return
+  // }
+
+
+  removeTaskType(task: TaskType) {
+    var index = this.tasktypes.indexOf(task);
+    this.tasktypes.splice(index, index);
+  }
 
 }
